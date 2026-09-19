d3355100125002bf095ac1bd21dd09cc0eb76d93
Samikshya Mishra <samikshya812@gmail.com>
2026-09-19 21:00:21 +0545

    Update function.js

---
+++ JS/function.js
@@ -0,0 +1,13 @@
+/*
+ * Function: Perfrom specific tasks
+ * 
+ * function <functionName> (params)? {
+ * //code
+ * }
+ * 
+*/
+
+function square(value){
+    console.log("square:", value * value);
+}
+square(6);
