3fc8775546e5bef96f54fd554f648182898e40b8
Samikshya Mishra <samikshya812@gmail.com>
2026-09-25 17:20:22 +0545

    commit

--- node/index.js
+++ node/index.js
@@ -1,4 +1,13 @@
+const fs = require("fs");
+
 console.log("Hello World");
 
 const name = "samikshya";
 console.log(`hello ${name}`);
+
+fs.readFile("text.txt", "uft-8", (error,data) =>{
+    if(error) throw error;
+
+    console.log(data);
+});
+
