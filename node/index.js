d6db2e6e0e2a3740f6ac2d3ef821f1e7f062731c
Samikshya Mishra <samikshya812@gmail.com>
2026-09-25 20:58:05 +0100

    updated

--- node/index.js
+++ node/index.js
@@ -1,13 +1,15 @@
 const fs = require("fs");
 
+//Non-blocking operation
+fs.readFile("text.txt", "uft-8", (error,data) =>{
+    if(error) throw error;
+
+    console.log(data);
+}); 
+
 console.log("Hello World");
 
 const name = "samikshya";
 console.log(`hello ${name}`);
 
-fs.readFile("text.txt", "uft-8", (error,data) =>{
-    if(error) throw error;
-
-    console.log(data);
-});
 
